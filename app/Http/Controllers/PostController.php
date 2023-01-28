2f3c9c9df9f0a84b68f53eebf029189201b1a575
tu0320 <s16c02000378@toyo.jp>
2023-01-28 17:37:35 +0900

    create表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,8 +15,12 @@ class PostController extends Controller
 
     public function show(Post $post)
 {
-    return view('posts/show')->with(['post' => $post]);
+        return view('posts/show')->with(['post' => $post]);
  //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
 }
+    public function create()
+    {
+        return view('posts/create');
+    }
 }
 ?>
